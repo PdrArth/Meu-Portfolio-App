804d67861db0dd78bad58e752ffd80f1092d46a0
Pedro Arthur <pedroarthurpdrarth@gmail.com>
2025-02-25 12:50:03 -0300

    first commit

---
+++ app/src/main/java/com/pdrarth/meupotifolio/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.pdrarth.meupotifolio
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.pdrarth.meupotifolio.databinding.ActivityMainBinding
+
+private lateinit var binding: ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        var projetc = binding.projetos
+        var contact = binding.contatos
+
+
+        projetc.setOnClickListener {
+            var intent =  Intent(this, Projetos::class.java)
+            startActivity(intent)
+
+
+        }
+        contact.setOnClickListener {
+            var intent =  Intent(this, Contato::class.java)
+            startActivity(intent)
+        }
+    }
+}
